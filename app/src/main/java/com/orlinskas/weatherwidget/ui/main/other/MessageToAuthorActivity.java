30fe58f3f2764c20836291458aade9421247bfa5
orlinskas <orlinskaslove@gmail.com>
2019-09-05 10:37:48 +0300

    добавлено меню

--- app/src/main/java/com/orlinskas/weatherwidget/ui/main/other/MessageToAuthorActivity.java
+++ app/src/main/java/com/orlinskas/weatherwidget/ui/main/other/MessageToAuthorActivity.java
@@ -1,15 +1,49 @@
 package com.orlinskas.weatherwidget.ui.main.other;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.orlinskas.weatherwidget.ActivityOpener;
 import com.orlinskas.weatherwidget.R;
+import com.orlinskas.weatherwidget.ToastBuilder;
 
 public class MessageToAuthorActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_to_author);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_repost:
+                Intent sendIntent = new Intent();
+                sendIntent.setType("text/plain");
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,  "\"Weather\" - удобный виджет погоды. Присоединяйся https://play.google.com/store/apps/developer?id=Orlinskas.Development");
+                startActivity(Intent.createChooser(sendIntent,"Поделиться"));
+                return true;
+            case R.id.action_help_pls:
+                ActivityOpener.openActivity(getApplicationContext(), HelpActivity.class);
+                return true;
+            case R.id.action_message:
+                ToastBuilder.create(getApplicationContext(), "Уже здесь");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
